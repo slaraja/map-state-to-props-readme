1801147ca8cadf85ad84ffbb4b840f29ffbe9aad
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-09 13:20:08 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 import './App.css';
 
 class App extends Component {
 	handleOnClick = () => {
-		this.props.store.dispatch({
+		this.props.dispatch({
+//dispatch is automatically provided by a prop if no arg2 provided (mapsDispatchToProps)
 			type: 'INCREASE_COUNT',
 		});
 	}
@@ -12,10 +14,18 @@ class App extends Component {
 		return (
 			<div className="App">
 				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
+				<p>{this.props.clicks}</p>
 			</div>
 		);
 	}
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return { clicks: state.clicks };
+}
+//specifies which piece of state we want to provide to comp
+//clicks is a prop
+
+export default connect(mapStateToProps)(App);
+//connect function is synced up to store, listening to each change in state
+
